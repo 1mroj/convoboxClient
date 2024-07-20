fe97eaf6a4b480f5ee9720dcf6cb0e4460556db9
Arivumathi R <60580194+arivusurya@users.noreply.github.com>
2024-07-20 20:52:38 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
 export const SERVER_URL = "https://skunk-capable-penguin.ngrok-free.app";
+export const FB_APP_ID = process.env.REACT_APP_FBID;
+
+export const FB_CONFIG_ID = process.env.REACT_APP_CONFIGID;
